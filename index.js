ce61c5bb94affb303498c47205296428ed25b92b
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-11 19:22:40 +0200

    add reducer

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ Action = {
 function buyCake() {
   return Action;
 }
+
+const initialState = {
+  numOfCakes: 10,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
