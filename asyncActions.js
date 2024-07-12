79639204b952b6915b2d37c332b2e9dfd4c1c44f
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-12 16:04:59 +0200

    fetch data using axios

--- asyncActions.js
+++ asyncActions.js
@@ -1,19 +1,23 @@
 const redux = require("redux");
-const axios = require('aaxios')
-const thunk= require('redux-thunk').default
-const applyMiddleware=redux.applyMiddleware
 const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require('redux-thunk').default;
+const axios = require("axios");
+
 const initialState = {
   loading: false,
   data: [],
   error: "",
 };
+
 const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
 const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
 const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+
 const fetchUsersRequest = () => {
   return { type: FETCH_USERS_REQUEST };
 };
+
 const fetchUsersSuccess = (users) => {
   return { type: FETCH_USERS_SUCCESS, payload: users };
 };
@@ -27,10 +31,30 @@ const reducer = (state = initialState, action) => {
     case FETCH_USERS_REQUEST:
       return { ...state, loading: true };
     case FETCH_USERS_SUCCESS:
-      return { loading: false, users: action.payload, error: "" };
+      return { loading: false, data: action.payload, error: "" };
     case FETCH_USERS_FAILURE:
-      return { loading: false, users: [], error: action.payload };
+      return { loading: false, data: [], error: action.payload };
+    default:
+      return state;
   }
 };
 
-const store = createStore(reducer,applyMiddleware(thunk));
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequest()); 
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((error) => {
+        dispatch(fetchUsersFailure(error.message));
+      });
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+// Dispatch the fetchUsers action to test
+store.dispatch(fetchUsers());
